f3d888539abdfcafc6c287392a157f9d6e7c9a57
aakx000 <aakx0506@gmail.com>
2024-05-19 15:15:30 +0300

    edited

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,40 +1,114 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-#include "example2.cpp"
 #include <string>
 
-class MockFan: public Fan {
-    public:
-    MockFan() : Fan() {}
+using namespace std;
+class command
+{
+public:
+   virtual void execute()=0;
+};
+class Fan
+{
+public:
+    void SetCircuitTringle()
+    {
+        std::cout<<"SetPower In Tringle toplogy circuit"<<std::endl;
+    }
+  void SetCircuitStar()
+  {
+      std::cout<<"SetPower In star toplogy circuit"<<std::endl;
+  }
+  void ConnectPower()
+  {
+      std::cout<<"Connect Power"<<std::endl;
+  }
+  void DisconnectPower()
+  {
+      std::cout<<"Disconnect Power"<<std::endl;
+  }
+  void Delay(int miliseconds)
+  {
+
+       std::cout<<"wait for "<<miliseconds<<" miliseconds"<<std::endl;
+  }
+};
+class TurnOnFanCommand:public command
+{
+    Fan *_fan;
+public:
+    TurnOnFanCommand(Fan*fan)
+    {
+        _fan=fan;
+    }
+
+    void execute() override
+    {
+        _fan->SetCircuitStar();
+        _fan->ConnectPower();
+        _fan->Delay(20000);
+        _fan->SetCircuitTringle();
+    }
+
+};
+class Controller
+{
+    command *_command;
+public:
+    Controller(){}
+    void SetCommand(command* command)
+    {
+        _command=command;
+    }
+    void ButtonClicked()
+    {
+        _command->execute();
+
+    }
+    ~Controller()
+    {
+        delete  _command;
+    }
+};
+class TurnOffFanCommand:public command
+{
+    Fan* _fan;
+public:
+    TurnOffFanCommand(Fan*fan){_fan=fan;}
+
+    void execute() override
+    {
+        _fan->DisconnectPower();
+    }
+
 };
 
 
-
-Test(MockFan, SetCircuitTringle) {
-    MockFan fan;
+Test(Fan, SetCircuitTringle) {
+    Fan fan;
     
     testing::internal::CaptureStdout();
-    fan.Fan::SetCircuitTringle();
+    fan.SetCircuitTringle();
     std::string output = testing::internal::GetCapturedStdout();
     EXPECT_EQ(output, "SetPower In Tringle toplogy circuit\n");
     
     testing::internal::CaptureStdout();
-    fan.Fan::SetCircuitStar();
+    fan.SetCircuitStar();
     output = testing::internal::GetCapturedStdout();
     EXPECT_EQ(output, "SetPower In star toplogy circuit\n");
     
     testing::internal::CaptureStdout();
-    fan.Fan::ConnectPower();
+    fan.ConnectPower();
     output = testing::internal::GetCapturedStdout();
     EXPECT_EQ(output, "Connect Power\n");
     
     testing::internal::CaptureStdout();
-    fan.Fan::DisconnectPower();
+    fan.DisconnectPower();
     output = testing::internal::GetCapturedStdout();
     EXPECT_EQ(output, "Disconnect Power\n");
     
     testing::internal::CaptureStdout();
-    fan.Fan::Delay(2000);
+    fan.Delay(2000);
     output = testing::internal::GetCapturedStdout();
     EXPECT_EQ(output, "wait for 2000 miliseconds\n");
 }
