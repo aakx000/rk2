c1ae3250bfef3137c99ff717865a92fe31accc36
aakx000 <aakx0506@gmail.com>
2024-05-19 15:35:13 +0300

    added gtest framework

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,89 +2,7 @@
 #include <gmock/gmock.h>
 #include <string>
 
-using namespace std;
-class command
-{
-public:
-   virtual void execute()=0;
-};
-class Fan
-{
-public:
-    void SetCircuitTringle()
-    {
-        std::cout<<"SetPower In Tringle toplogy circuit"<<std::endl;
-    }
-  void SetCircuitStar()
-  {
-      std::cout<<"SetPower In star toplogy circuit"<<std::endl;
-  }
-  void ConnectPower()
-  {
-      std::cout<<"Connect Power"<<std::endl;
-  }
-  void DisconnectPower()
-  {
-      std::cout<<"Disconnect Power"<<std::endl;
-  }
-  void Delay(int miliseconds)
-  {
-
-       std::cout<<"wait for "<<miliseconds<<" miliseconds"<<std::endl;
-  }
-};
-class TurnOnFanCommand:public command
-{
-    Fan *_fan;
-public:
-    TurnOnFanCommand(Fan*fan)
-    {
-        _fan=fan;
-    }
-
-    void execute() override
-    {
-        _fan->SetCircuitStar();
-        _fan->ConnectPower();
-        _fan->Delay(20000);
-        _fan->SetCircuitTringle();
-    }
-
-};
-class Controller
-{
-    command *_command;
-public:
-    Controller(){}
-    void SetCommand(command* command)
-    {
-        _command=command;
-    }
-    void ButtonClicked()
-    {
-        _command->execute();
-
-    }
-    ~Controller()
-    {
-        delete  _command;
-    }
-};
-class TurnOffFanCommand:public command
-{
-    Fan* _fan;
-public:
-    TurnOffFanCommand(Fan*fan){_fan=fan;}
-
-    void execute() override
-    {
-        _fan->DisconnectPower();
-    }
-
-};
-
-
-Test(Fan, SetCircuitTringle) {
+TEST(Fan, SetCircuitTringle) {
     Fan fan;
     
     testing::internal::CaptureStdout();
@@ -113,7 +31,7 @@ Test(Fan, SetCircuitTringle) {
     EXPECT_EQ(output, "wait for 2000 miliseconds\n");
 }
 
-Test(Controller, TurnOnFanCommand) {
+TEST(Controller, TurnOnFanCommand) {
     Controller controller;
     Fan fan;
     controller.SetCommand(new TurnOnFanCommand(&fan));
@@ -123,7 +41,7 @@ Test(Controller, TurnOnFanCommand) {
     EXPECT_EQ(output, "SetPower In star toplogy circuit\nConnect Power\nwait for 20000 miliseconds\nSetPower In Tringle toplogy circuit\n");
 }
 
-Test(Controller, TurnOffFanCommand) {
+TEST(Controller, TurnOffFanCommand) {
     Controller controller;
     Fan fan;
     controller.SetCommand(new TurnOnFanCommand(&fan));
@@ -132,3 +50,4 @@ Test(Controller, TurnOffFanCommand) {
     std::string output = testing::internal::GetCapturedStdout();
     EXPECT_EQ(output, "Disconnect Power\n");
 }
+
